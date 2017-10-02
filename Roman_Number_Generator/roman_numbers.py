a7eabc60a31d1c48225cc3d470621b99982bafa7
Amartya Gupta <amartyazzz@gmail.com>
2017-10-02 22:59:36 +0530

    Error correction

--- Roman_Number_Generator/roman_numbers.py
+++ Roman_Number_Generator/roman_numbers.py
@@ -1,29 +1,50 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Created on Wed Sep 27 16:33:18 2017
+Created on Wed Sep 27 22:02:44 2017
 
-@author: amartya
+@author: Amartya Gupta
 """
 
-"""TODO: create a RomanNumerals helper object"""
-
-class RomanNumerals(object):
-    base = {
-                0: {"I": 1, 1: "I",
+base = {
+                1: {"I": 1, 1: "I",
                     "V": 5, 5: "V"},
-                1: {"X": 10, 10: "X",
+                2: {"X": 10, 10: "X",
                     "L": 50, 50: "L"},
-                2: {"C": 100, 100: "C",
+                3: {"C": 100, 100: "C",
                     "D": 500, 500: "D"},
-                3: {"M": 1000, 1000: "M"}
+                4: {"M": 1000, 1000: "M",
+                    "V_": 5000, 5000: "V_"}
             }
-    
-    def __init__(self, num):
-        self.num = num
-    
-    def to_roman(self):
-        pass
-    
-    def from_roman(self):
-        pass
+
+        
+def logic(n, s):
+    if s > 4: 
+        return "outrange"
+    if n in base[s]:
+        return base[s][n]
+    else:
+        mi = min(i for i in base[s] if type(i) is int)
+        mx = mi * 5
+        if n < mx:
+            if n // mi <= 3:
+                return base[s][mi] + logic(n - base[s][base[s][mi]], s)
+            else:
+                return base[s][mi] + logic(n + base[s][base[s][mi]], s)
+        else:
+            if (n - mx) // mi <= 3:
+                return base[s][mx] + logic(n - base[s][base[s][mx]], s)
+            else:
+                return base[s][mi] + logic(n + base[s][base[s][mi]], s+1)
+            
+def solution(n):
+    # TODO convert int to roman string
+    result = []
+    for i in range(1, len(str(n)) + 1):
+        temp = n % 10**i
+        if temp:
+            n -= temp
+            result.append(logic(n=temp, s=i))
+    return ''.join(result[::-1])
+        
+        
